94ecdf919e3f8048c75ff61cd0a0f2d7885d21cc
Tim <hello@timsmart.co>
2025-02-27 15:20:36 +1300

    wip

--- src/Sql.ts
+++ src/Sql.ts
@@ -1,10 +1,11 @@
-import { MysqlClient } from "@effect/sql-mysql2"
+import { PgClient } from "@effect/sql-pg"
 import { Config } from "effect"
+import { constVoid } from "effect/Function"
 
-export const SqlLayer = MysqlClient.layerConfig({
+export const SqlLayer = PgClient.layerConfig({
   database: Config.string("DB_DATABASE"),
   username: Config.string("DB_USER"),
   password: Config.redacted("DB_PASSWORD"),
-  host: Config.string("DB_HOST"),
-  port: Config.integer("DB_PORT"),
+  host: Config.succeed("postgres.postgres.svc"),
+  onnotice: Config.succeed(constVoid),
 })
--- src/pod.ts
+++ src/pod.ts
@@ -1,7 +1,7 @@
 import { Entity, Singleton } from "@effect/cluster"
 import { NodeClusterSocketPods, NodeRuntime } from "@effect/platform-node"
 import { Rpc } from "@effect/rpc"
-import { Array, Effect, Layer, Logger, LogLevel, Schema } from "effect"
+import { Array, Effect, Layer, Logger, Mailbox, Schema, Stream } from "effect"
 import { SqlLayer } from "./Sql"
 
 const Counter = Entity.make("Counter", [
@@ -14,24 +14,26 @@ const Counter = Entity.make("Counter", [
     payload: { amount: Schema.Number },
     success: Schema.Number,
   }),
+
+  Rpc.make("Stream", {
+    success: Schema.Number,
+    stream: true,
+  }),
 ])
 
 const CounterLive = Counter.toLayer(
   Effect.gen(function* () {
     const podAddress = yield* Entity.CurrentPodAddress
     const address = yield* Entity.CurrentAddress
-    console.log("Creating Counter", address)
+    yield* Effect.annotateLogs(Effect.log("Creating Counter"), {
+      address,
+      pod: podAddress,
+    })
 
     let state = 0
 
-    yield* Effect.addFinalizer(() => Effect.log("Finalizing", state))
-
     return {
-      Increment: Effect.fnUntraced(function* ({
-        payload: { amount },
-        requestId,
-      }) {
-        console.log("Incrementing by", amount, requestId, podAddress)
+      Increment: Effect.fnUntraced(function* ({ payload: { amount } }) {
         // yield* Effect.sleep(1000)
         state += amount
         return state
@@ -42,33 +44,60 @@ const CounterLive = Counter.toLayer(
         state -= amount
         return state
       }),
+      Stream: Effect.fnUntraced(function* () {
+        const mailbox = yield* Mailbox.make<number>()
+
+        let i = 0
+        yield* Effect.suspend(() => mailbox.offer(i++)).pipe(
+          Effect.andThen(Effect.sleep(1000)),
+          Effect.forever,
+          Effect.forkScoped,
+        )
+
+        return mailbox
+      }),
     }
   }),
-  { maxIdleTime: "10 seconds", concurrency: 1000 },
+  { maxIdleTime: "10 seconds", concurrency: 100 },
 )
 
-const SendMessages = Array.makeBy(10, (i) =>
+const SendMessages = Array.makeBy(1, (i) =>
   Singleton.make(
     `SendMessage${i}`,
     Effect.gen(function* () {
       const makeClient = yield* Counter.client
       const semaphore = yield* Effect.makeSemaphore(50)
-      const clients = Array.makeBy(100, (i) => makeClient(`client-${i}`))
+      const clients = Array.makeBy(1000, (i) => makeClient(`client-${i}`))
       console.log("SendMessages started")
       for (let i = 0; true; i++) {
         const client = clients[i % clients.length]
         yield* semaphore.take(1)
-        yield* client.Increment({ amount: 1 }).pipe(
-          Effect.flatMap((result) => Effect.log(`Client ${i}`, result)),
-          Effect.ensuring(semaphore.release(1)),
-          Effect.fork,
+        yield* client
+          .Increment({ amount: 1 })
+          .pipe(Effect.ensuring(semaphore.release(1)), Effect.fork)
+      }
+    }),
+  ),
+)
+
+const SendStreams = Array.makeBy(30, (i) =>
+  Singleton.make(
+    `SendStreams${i}`,
+    Effect.gen(function* () {
+      const makeClient = yield* Counter.client
+      console.log("SendStreams started")
+      for (let i = 0; i < 10; i++) {
+        const client = makeClient(`client-${i}`)
+        yield* client.Stream().pipe(
+          Stream.runForEach((i) => Effect.log("stream", i)),
+          Effect.forkScoped,
         )
       }
     }),
   ),
 )
 
-const Entities = Layer.mergeAll(CounterLive, ...SendMessages)
+const Entities = Layer.mergeAll(CounterLive, ...SendMessages, ...SendStreams)
 
 const ShardingLive = NodeClusterSocketPods.layer({
   storage: "sql",
@@ -76,7 +105,7 @@ const ShardingLive = NodeClusterSocketPods.layer({
 
 Entities.pipe(
   Layer.provide(ShardingLive),
-  Layer.provide(Logger.minimumLogLevel(LogLevel.All)),
+  Layer.provide(Logger.json),
   Layer.launch,
-  NodeRuntime.runMain,
+  NodeRuntime.runMain({ disablePrettyLogger: true }),
 )
